82563dbf09df2ec8dce92de8b8f8053c40ac61b2
Hubert Kindermann <hubert.kindermann@hotmail.com>
2016-07-07 13:28:21 +0200

    Syntax higlighting uses default theme colors. Fixes #12.

--- VsTeXCommentsExtension/SyntaxHighlighting/ClassificationDefinitions.cs
+++ VsTeXCommentsExtension/SyntaxHighlighting/ClassificationDefinitions.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.VisualStudio.Text.Classification;
+﻿using Microsoft.VisualStudio.Language.StandardClassification;
+using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 using System.ComponentModel.Composition;
-using System.Windows.Media;
 
 namespace VsTeXCommentsExtension.SyntaxHighlighting
 {
@@ -9,43 +9,33 @@ namespace VsTeXCommentsExtension.SyntaxHighlighting
     {
         #region Classification Type Definitions
         [Export]
-        [Name("TeX")]
-        internal static ClassificationTypeDefinition TeXClassificationDefinition = null;
+        [Name("TeX.Command")]
+        [BaseDefinition(PredefinedClassificationTypeNames.Keyword)]
+        internal static ClassificationTypeDefinition TeXCommandDefinition = null;
 
         [Export]
-        [Name("TeX.command")]
-        [BaseDefinition("TeX")]
-        internal static ClassificationTypeDefinition TeXAddedDefinition = null;
-
-        [Export]
-        [Name("TeX.mathBlock")]
-        [BaseDefinition("TeX")]
-        internal static ClassificationTypeDefinition TeXRemovedDefinition = null;
+        [Name("TeX.MathBlock")]
+        [BaseDefinition(PredefinedClassificationTypeNames.String)]
+        internal static ClassificationTypeDefinition TeXMathBlockDefinition = null;
         #endregion
 
         #region Classification Format Productions
         [Export(typeof(EditorFormatDefinition))]
-        [ClassificationType(ClassificationTypeNames = "TeX.command")]
-        [Name("TeX.command")]
+        [ClassificationType(ClassificationTypeNames = "TeX.Command")]
+        [Name("TeX.Command")]
         [Order(After = Priority.High)]
-        internal sealed class TeXAddedFormat : ClassificationFormatDefinition
+        [UserVisible(true)]
+        internal sealed class TeXCommandFormat : ClassificationFormatDefinition
         {
-            public TeXAddedFormat()
-            {
-                ForegroundColor = Colors.Blue;
-            }
         }
 
         [Export(typeof(EditorFormatDefinition))]
-        [ClassificationType(ClassificationTypeNames = "TeX.mathBlock")]
-        [Name("TeX.mathBlock")]
+        [ClassificationType(ClassificationTypeNames = "TeX.MathBlock")]
+        [Name("TeX.MathBlock")]
         [Order(After = Priority.High)]
-        internal sealed class TeXRemovedFormat : ClassificationFormatDefinition
+        [UserVisible(true)]
+        internal sealed class TeXMathBlockFormat : ClassificationFormatDefinition
         {
-            public TeXRemovedFormat()
-            {
-                ForegroundColor = Colors.Red;
-            }
         }
         #endregion
     }
